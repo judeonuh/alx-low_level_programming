137f43072302f51e037bfec83e6f6ef5aa7398b0
judeprince <judeprinceonuh@gmail.com>
2022-09-09 07:08:43 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ * * Description
+ *
+ * */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z') {
+	        putchar (ch);
+	        ++ch;
+	}
+	putchar('\n');
+
+        return (0);
+}
